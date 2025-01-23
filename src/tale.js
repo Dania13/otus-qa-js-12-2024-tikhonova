a073dc962d373ff14f1e21e9a58b297b90f0d766
Dasha <dashtikhrambler.rudashtikhrambler.ru>
2025-01-23 13:11:48 +0300

    add import/export

--- src/tale.js
+++ src/tale.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-const kolobok = (nameHero) => {
+export const kolobok = (nameHero) => {
   switch (nameHero) {
     case 'дедушка':
       return 'Я от дедушки ушёл';
@@ -7,11 +6,11 @@ const kolobok = (nameHero) => {
       return 'Я от зайца ушёл';
     case 'лиса':
       return 'Меня съели';
+    default:
+      return `Не встречал ${nameHero}`;
   }
-  return 'Не встречал';
 };
 
-// eslint-disable-next-line no-unused-vars
-const newYear = (hero) => {
+export const newYear = (hero) => {
   return `${hero}! ${hero}! ${hero}!`;
 };
