ed70b0b4cc512e56e903e0183304fc9ef6cf856c
Dasha <dashtikhrambler.rudashtikhrambler.ru>
2025-01-24 22:32:58 +0300

    add jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginJest from 'eslint-plugin-jest';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
+  pluginJest,
   {
     ignores: ['node_modules'],
   },
