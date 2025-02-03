92e85682ef436cb0849cb06e3eb2786e0ea4170f
The Who <dashtikh@rambler.ru>
2025-02-03 10:35:21 +0300

    add secret variables

--- framework/config/config.js
+++ framework/config/config.js
@@ -1,10 +1,12 @@
+import 'dotenv/config';
 import { faker } from '@faker-js/faker';
+
 const name = faker.person.fullName();
 
 export const config = {
-  baseURL: process.env.BOOKSTORE_BASE_URL,
+  baseURL: process.env.TEST_BOOKSTORE_BASE_URL,
   credentials: {
     userName: `${name}`,
-    password: process.env.BOOKSTORE_PASSWORD,
+    password: process.env.TEST_BOOKSTORE_PASSWORD,
   },
 };
