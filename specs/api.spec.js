56f484d23d42cd2ca7429fdbc4cc8c10cd8eeeee
The Who <dashtikh@rambler.ru>
2025-02-07 17:27:19 +0300

    refactor api tests

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -1,32 +1,22 @@
 import { config } from '../framework/config/config';
 import { generateUserCredentials } from '../framework/fixtures/userFixtures';
+import {
+  createUser,
+  deleteUser,
+  generateToken,
+} from '../framework/services/UserBookService';
 
 describe('bookstore tests', () => {
-  const baseURL = config.baseURL;
-
   it('create existing user', async () => {
     let user = {
-      userName: `${config.credentials.userName}1`,
+      userName: `${config.credentials.userName}`,
       password: `${config.credentials.password}`,
     };
 
-    await fetch(`${baseURL}/User`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
-    const response = await fetch(`${baseURL}/User`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
-    const answer = await response.json();
-    expect(answer.message).toBe('User exists!');
-    expect(answer.code).toBe('1204');
+    const response = await createUser(user);
+
+    expect(response.data.message).toBe('User exists!');
+    expect(response.data.code).toBe('1204');
   });
   it('create user with non valid password', async () => {
     let user = {
@@ -34,107 +24,45 @@ describe('bookstore tests', () => {
       password: 'Qwerty',
     };
 
-    const response = await fetch(`${baseURL}/User`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
-    const answer = await response.json();
-    expect(answer.message).toBe(
+    const response = await createUser(user);
+
+    expect(response.data.message).toBe(
       "Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer.",
     );
-    expect(answer.code).toBe('1300');
+    expect(response.data.code).toBe('1300');
   });
   it('create user success', async () => {
-    let user = {
-      userName: `${generateUserCredentials().userName}`,
-      password: `${generateUserCredentials().password}`,
-    };
-    const response = await fetch(`${baseURL}/User`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
-    const answer = await response.json();
-    expect(answer.username).toBe(`${user.userName}`);
-    expect(answer.userID).not.toBe(undefined);
-    expect(typeof answer.userID).toBe('string');
+    let user = generateUserCredentials();
+    const response = await createUser(user);
 
-    const userID = answer.userID;
-    try {
-      await fetch(`${baseURL}/GenerateToken`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    expect(response.data.username).toBe(`${user.userName}`);
+    const userID = response.data.userID;
+    expect(userID).not.toBe(undefined);
+    expect(typeof response.data.userID).toBe('string');
 
-    try {
-      (async () => {
-        const auth =
-          'Basic ' +
-          Buffer.from(user.userName + ':' + user.password).toString('base64');
-        const myHeaders = new Headers();
-        myHeaders.append('Authorization', auth);
-        await fetch(`${baseURL}/User/${userID}`, {
-          method: 'DELETE',
-          headers: myHeaders,
-          redirect: 'follow',
-        });
-      })();
-    } catch (error) {
-      console.error(error);
-    }
+    const token = (await generateToken(user)).data.token;
+    await deleteUser(userID, token);
   });
   it('generate token with error', async () => {
     let user = {
       userName: `${generateUserCredentials().userName}`,
     };
 
-    const response = await fetch(`${baseURL}/GenerateToken`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
-    const answer = await response.json();
-    expect(answer.code).toBe('1200');
-    expect(answer.message).toBe('UserName and Password required.');
+    const response = await generateToken(user);
+
+    expect(response.data.code).toBe('1200');
+    expect(response.data.message).toBe('UserName and Password required.');
   });
 
   it('generate token success', async () => {
-    let user = {
-      userName: `${generateUserCredentials().userName}`,
-      password: `${generateUserCredentials().password}`,
-    };
+    let user = generateUserCredentials();
 
-    await fetch(`${baseURL}/User`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
+    await createUser(user);
 
-    const response = await fetch(`${baseURL}/GenerateToken`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
-    const answer = await response.json();
-    expect(answer.token).not.toBe(undefined);
-    expect(typeof answer.token).toBe('string');
-    expect(answer.status).toBe('Success');
+    const response = await generateToken(user);
+
+    expect(response.data.token).not.toBe(undefined);
+    expect(typeof response.data.token).toBe('string');
+    expect(response.data.status).toBe('Success');
   });
 });
